bdd9b0612190c92c51d2afa20b91208633f6831a
Muhammad Abu Huraira <huraira.eqeel@gmail.com>
2025-04-18 09:06:16 +0500

    lines changes

--- task_manager/app/src/main/java/com/example/task_manager/MainActivity.java
+++ task_manager/app/src/main/java/com/example/task_manager/MainActivity.java
@@ -1,117 +1,66 @@
 package com.example.task_manager;
 
-
-
 import android.Manifest;
-
 import android.content.Intent;
-
 import android.content.pm.PackageManager;
-
 import android.database.Cursor;
-
 import android.database.sqlite.SQLiteDatabase;
-
 import android.net.Uri;
-
 import android.os.Build;
-
 import android.os.Bundle;
-
 import android.os.Environment;
-
 import android.provider.Settings;
-
 import android.util.Log;
-
 import android.view.MenuItem;
-
 import android.widget.ImageView;
-
 import android.widget.Toast;
 
 
 
 import androidx.activity.EdgeToEdge;
-
 import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.core.app.ActivityCompat;
-
 import androidx.core.content.ContextCompat;
-
 import androidx.fragment.app.Fragment;
-
-
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-
-
 import java.text.SimpleDateFormat;
-
 import java.util.Calendar;
-
 import java.util.Date;
-
 import java.util.Locale;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
-
     private static final int STORAGE_PERMISSION_REQUEST_CODE = 4;
 
     private static final int MANAGE_STORAGE_PERMISSION_REQUEST_CODE = 5;
 
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
-
         setContentView(R.layout.activity_main);
-
         requestStoragePermission();
-
         db_class db = new db_class(this, "MYDATABASE", null, 1);
-
         SQLiteDatabase database = db.getReadableDatabase(); // Forces the database to open
 
         Log.d("DB_CHECK", "Database opened successfully!");
 
-
-
         Cursor cursor2 = db.getAllTasks();
-
         while (cursor2.moveToNext()) {
-
             String taskDate = cursor2.getString(cursor2.getColumnIndexOrThrow("date"));
-
             String taskTime = cursor2.getString(cursor2.getColumnIndexOrThrow("time"));
-
             String taskTitle = cursor2.getString(cursor2.getColumnIndexOrThrow("title"));
-
             String taskDescription = cursor2.getString(cursor2.getColumnIndexOrThrow("description"));
-
-
 
             // Check if task is due now
 
             if (isTaskDueNow(taskDate, taskTime)) {
-
                 Intent serviceIntent = new Intent(MainActivity.this, Notification.class);
-
                 serviceIntent.putExtra("task_title", taskTitle);
-
                 serviceIntent.putExtra("task_description", taskDescription);
-
                 serviceIntent.putExtra("task_time", taskTime);
 
                 startService(serviceIntent);
@@ -122,33 +71,15 @@ public class MainActivity extends AppCompatActivity {
 
         cursor2.close();
 
-
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-
-
-
 
         // Set default fragment when app starts
 
         loadFragment(new HomeFragment());
-
-
-
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-
-
-
-
-
             @Override
-
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 Fragment selectedFragment = null;
-
-
 
                 if (item.getItemId() == R.id.nav_home) {
 
@@ -177,15 +108,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-
-
-
-
-
-
-
-
-
         });
 
     }
@@ -198,15 +120,11 @@ public class MainActivity extends AppCompatActivity {
 
             Date taskDateTime = sdf.parse(taskDate + " " + taskTime);
 
-
-
             Calendar now = Calendar.getInstance();
 
             long currentTime = now.getTimeInMillis();
 
             long taskTimeInMillis = taskDateTime.getTime();
-
-
 
             // Check if task is due in 30 minutes (1800000 ms)
 
@@ -225,9 +143,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
     private void loadFragment(Fragment fragment) {
 
         getSupportFragmentManager().beginTransaction()
@@ -242,22 +157,18 @@ public class MainActivity extends AppCompatActivity {
 
     private void requestStoragePermission() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {  // Android 11+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
 
             if (!android.os.Environment.isExternalStorageManager()) {
 
                 // Show permission request toast
-
                 Toast.makeText(this, "Requesting Manage Storage Permission", Toast.LENGTH_SHORT).show();
 
 
 
                 // Open settings to grant permission
-
                 Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-
                 intent.setData(Uri.parse("package:" + getPackageName()));
-
                 startActivityForResult(intent, MANAGE_STORAGE_PERMISSION_REQUEST_CODE);
 
             } else {
@@ -272,16 +183,13 @@ public class MainActivity extends AppCompatActivity {
 
                     ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
-
-
-                ActivityCompat.requestPermissions(this, new String[]{
+                   ActivityCompat.requestPermissions(this, new String[]{
 
                         Manifest.permission.READ_EXTERNAL_STORAGE,
 
                         Manifest.permission.WRITE_EXTERNAL_STORAGE
 
                 }, STORAGE_PERMISSION_REQUEST_CODE);
-
 
 
                 Toast.makeText(this, "Requesting storage permissions", Toast.LENGTH_SHORT).show();
@@ -297,25 +205,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
-
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-
-
         if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
-
             if (grantResults.length > 0) {
-
                 boolean readGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-
                 boolean writeGranted = grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-
-
                 if (readGranted && writeGranted) {
 
                     Toast.makeText(this, "Storage permission granted", Toast.LENGTH_SHORT).show();
